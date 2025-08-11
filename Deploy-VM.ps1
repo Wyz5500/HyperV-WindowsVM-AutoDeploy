1cb31128955d26128dbf0dbcff341dfaf226c508
Wyz5500 <1321515855@qq.com>
2025-08-11 22:04:42 +0800

    支持在脚本相同目录扫描并载入"autounattend.xml"作为应答文件，不再提供用户名和密码设置

--- Deploy-VM.ps1
+++ Deploy-VM.ps1
@@ -1,241 +1,45 @@
-#设置虚拟机名称：
 $vmName = "Windows VM"
-
-#设置cpu核心数，不能超过主机逻辑核心数。
 $cpuCore = '4'
-
-#设置虚拟网络交换机
 $switchName = "Default Switch"
-
-#设置保存虚拟硬盘的目录，末尾要有'\'。
 $VHDXDirPath = "C:\ProgramData\Microsoft\Windows\Virtual Hard Disks\"
-
-#设置虚拟机虚拟硬盘最大空间，不能超过2048TB，单位可以是'GB'、'TB'。
 $VHDXSize = "1TB"
-
-#设置Windows安装镜像位置。
 $isoPath = ""
-
-#设置索引，用于指定Windows版本。微软官网下载的多版本镜像专业版为4，专业工作站版为6，可以用Dism++查看Windows版本对应的索引。或者使用命令："Dism /Get-ImageInfo /ImageFile:<install.wim/install.esd文件所在路径>"。
-$index = 4
-
-#设置Windows用户名，不想选就用"$env:USERNAME"，即当前用户的用户名。可以自己修改，要加双引号。
-$userName = "$env:USERNAME"
-
-#设置Windows用户登录密码。
-$password = ""
-
-#设置为虚拟机启动时分配的内存，不能超过物理机内存上限，一般不需要设置
-#$ram = 8GB
+$index = 1
 
 ##################################################
 #只有上面需要设置，下面的东西不要乱动
 ##################################################
+
+#自动搜索脚本所在目录下的"unattend.xml"作为应答文件
 function unattendProcess {
-    $tempXmlPath = "$($PSScriptRoot)\fihbfaygfwiygfiuawgfuawygf.xml"
-    $targetDirPath = "$($ntfsDriverLetter):\Windows\Panther\"
-    $targetPath = "$($targetDirPath)Unattend.xml"
-
-    $xmlContent = @'
-<?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
-	<settings pass="offlineServicing"></settings>
-	<settings pass="windowsPE">
-		<component name="Microsoft-Windows-International-Core-WinPE" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-			<SetupUILanguage>
-				<UILanguage>zh-CN</UILanguage>
-			</SetupUILanguage>
-			<InputLocale>0804:{81d4e9c9-1d3b-41bc-9e6c-4b40bf79e35e}{fa550b04-5ad7-411f-a5ac-ca038ec515d7};0409:00000409</InputLocale>
-			<SystemLocale>zh-Hans-CN</SystemLocale>
-			<UILanguage>zh-CN</UILanguage>
-			<UserLocale>zh-Hans-CN</UserLocale>
-		</component>
-		<component name="Microsoft-Windows-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-			<UserData>
-				<ProductKey>
-					<Key>00000-00000-00000-00000-00000</Key>
-					<WillShowUI>Always</WillShowUI>
-				</ProductKey>
-				<AcceptEula>true</AcceptEula>
-			</UserData>
-			<UseConfigurationSet>false</UseConfigurationSet>
-		</component>
-	</settings>
-	<settings pass="generalize"></settings>
-	<settings pass="specialize">
-		<component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-			<RunSynchronous>
-				<RunSynchronousCommand wcm:action="add">
-					<Order>1</Order>
-					<Path>powershell.exe -WindowStyle Normal -NoProfile -Command "$xml = [xml]::new(); $xml.Load('C:\Windows\Panther\unattend.xml'); $sb = [scriptblock]::Create( $xml.unattend.Extensions.ExtractScript ); Invoke-Command -ScriptBlock $sb -ArgumentList $xml;"</Path>
-				</RunSynchronousCommand>
-				<RunSynchronousCommand wcm:action="add">
-					<Order>2</Order>
-					<Path>powershell.exe -WindowStyle Normal -NoProfile -Command "Get-Content -LiteralPath 'C:\Windows\Setup\Scripts\Specialize.ps1' -Raw | Invoke-Expression;"</Path>
-				</RunSynchronousCommand>
-			</RunSynchronous>
-		</component>
-	</settings>
-	<settings pass="auditSystem"></settings>
-	<settings pass="auditUser"></settings>
-	<settings pass="oobeSystem">
-		<component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-			<InputLocale>0804:{81d4e9c9-1d3b-41bc-9e6c-4b40bf79e35e}{fa550b04-5ad7-411f-a5ac-ca038ec515d7};0409:00000409</InputLocale>
-			<SystemLocale>zh-Hans-CN</SystemLocale>
-			<UILanguage>zh-CN</UILanguage>
-			<UserLocale>zh-Hans-CN</UserLocale>
-		</component>
-		<component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-			<UserAccounts>
-				<LocalAccounts>
-					<LocalAccount wcm:action="add">
-						<Name>${userName}</Name>
-						<DisplayName></DisplayName>
-						<Group>Administrators</Group>
-						<Password>
-							<Value>${password}</Value>
-							<PlainText>true</PlainText>
-						</Password>
-					</LocalAccount>
-				</LocalAccounts>
-			</UserAccounts>
-			<AutoLogon>
-				<Username>${userName}</Username>
-				<Enabled>true</Enabled>
-				<LogonCount>1</LogonCount>
-				<Password>
-					<Value>${password}</Value>
-					<PlainText>true</PlainText>
-				</Password>
-			</AutoLogon>
-			<OOBE>
-				<ProtectYourPC>3</ProtectYourPC>
-				<HideEULAPage>true</HideEULAPage>
-				<HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
-				<HideOnlineAccountScreens>false</HideOnlineAccountScreens>
-			</OOBE>
-			<FirstLogonCommands>
-				<SynchronousCommand wcm:action="add">
-					<Order>1</Order>
-					<CommandLine>powershell.exe -WindowStyle Normal -NoProfile -Command "Get-Content -LiteralPath 'C:\Windows\Setup\Scripts\FirstLogon.ps1' -Raw | Invoke-Expression;"</CommandLine>
-				</SynchronousCommand>
-			</FirstLogonCommands>
-		</component>
-	</settings>
-	<Extensions xmlns="https://schneegans.de/windows/unattend-generator/">
-		<ExtractScript>
-param(
-    [xml] $Document
-);
-
-foreach( $file in $Document.unattend.Extensions.File ) {
-    $path = [System.Environment]::ExpandEnvironmentVariables( $file.GetAttribute( 'path' ) );
-    mkdir -Path( $path | Split-Path -Parent ) -ErrorAction 'SilentlyContinue';
-    $encoding = switch( [System.IO.Path]::GetExtension( $path ) ) {
-        { $_ -in '.ps1', '.xml' } { [System.Text.Encoding]::UTF8; }
-        { $_ -in '.reg', '.vbs', '.js' } { [System.Text.UnicodeEncoding]::new( $false, $true ); }
-        default { [System.Text.Encoding]::Default; }
-    };
-    $bytes = $encoding.GetPreamble() + $encoding.GetBytes( $file.InnerText.Trim() );
-    [System.IO.File]::WriteAllBytes( $path, $bytes );
-}
-		</ExtractScript>
-		<File path="C:\Windows\Setup\Scripts\Specialize.ps1">
-$scripts = @(
-	{
-		net.exe accounts /maxpwage:UNLIMITED;
-	};
-);
-
-&amp; {
-  [float] $complete = 0;
-  [float] $increment = 100 / $scripts.Count;
-  foreach( $script in $scripts ) {
-    Write-Progress -Activity 'Running scripts to customize your Windows installation. Do not close this window.' -PercentComplete $complete;
-    '*** Will now execute command &#xAB;{0}&#xBB;.' -f $(
-      $str = $script.ToString().Trim() -replace '\s+', ' ';
-      $max = 100;
-      if( $str.Length -le $max ) {
-        $str;
-      } else {
-        $str.Substring( 0, $max - 1 ) + '&#x2026;';
-      }
-    );
-    $start = [datetime]::Now;
-    &amp; $script;
-    '*** Finished executing command after {0:0} ms.' -f [datetime]::Now.Subtract( $start ).TotalMilliseconds;
-    "`r`n" * 3;
-    $complete += $increment;
-  }
-} *&gt;&amp;1 &gt;&gt; "C:\Windows\Setup\Scripts\Specialize.log";
-		</File>
-		<File path="C:\Windows\Setup\Scripts\FirstLogon.ps1">
-$scripts = @(
-	{
-		Set-ItemProperty -LiteralPath 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name 'AutoLogonCount' -Type 'DWord' -Force -Value 0;
-	};
-);
-
-&amp; {
-  [float] $complete = 0;
-  [float] $increment = 100 / $scripts.Count;
-  foreach( $script in $scripts ) {
-    Write-Progress -Activity 'Running scripts to finalize your Windows installation. Do not close this window.' -PercentComplete $complete;
-    '*** Will now execute command &#xAB;{0}&#xBB;.' -f $(
-      $str = $script.ToString().Trim() -replace '\s+', ' ';
-      $max = 100;
-      if( $str.Length -le $max ) {
-        $str;
-      } else {
-        $str.Substring( 0, $max - 1 ) + '&#x2026;';
-      }
-    );
-    $start = [datetime]::Now;
-    &amp; $script;
-    '*** Finished executing command after {0:0} ms.' -f [datetime]::Now.Subtract( $start ).TotalMilliseconds;
-    "`r`n" * 3;
-    $complete += $increment;
-  }
-} *&gt;&amp;1 &gt;&gt; "C:\Windows\Setup\Scripts\FirstLogon.log";
-		</File>
-	</Extensions>
-</unattend>
-'@
-
-    #创建临时文件，用于将原内容替换为设置的内容
-    Set-Content -Path $tempXmlPath -Value $xmlContent -Encoding utf8
-    $result = Get-Content -LiteralPath $tempXmlPath | ForEach-Object {
-        $line = $_
-        $line = $line.Replace('${userName}',"$userName")
-        $line = $line.Replace('${password}',"$password")
-        #$line = $line.Replace('${vmName}', "$vmName")
-        $line
+    $files = Get-ChildItem -Path $PSScriptRoot
+    foreach ($file in $files) {
+        if ($file.Name -eq "autounattend.xml") {
+            "在$($PSScriptRoot)中找到应答文件……"
+            New-Item -Path "$($ntfsDriverLetter):\Windows\Panther\" -ItemType Directory
+            $unattendInfo = Copy-Item -Path "$($PSScriptRoot)\autounattend.xml" -Destination "$($ntfsDriverLetter):\Windows\Panther\unattend.xml"
+        }
     }
-
-    #将替换后的内容写入应答文件
-    "写入应答文件……"
-    $unattendDir = New-Item -Path $targetDirPath -ItemType Directory
-    $unattendFile = New-item -Path $targetPath
-    Set-Content -LiteralPath $targetPath -Value $result
-
-    #删除临时文件
-    Remove-Item -Path $tempXmlPath
 }
 
 function startVM {
     #新建虚拟机
-    New-VM -Name $vmName `
-    -Generation 2 `
-    -SwitchName $SwitchName `
-    -VHDPath $VHDXPath `
-    #-MemoryStartupBytes $ram `
+    $VM = @{
+        Name = $vmName
+        Generation = 2
+        SwitchName = $SwitchName
+        VHDPath = $VHDXPath
+    }
+    New-VM @VM
 
-    #设置虚拟机参数
-    Set-VMProcessor -VMName $vmName -Count $cpuCore
+    #调整虚拟机设置
     $VMHD = Get-VMHardDiskDrive -VMName $vmName
     Set-VMFirmware -VMName $vmName -FirstBootDevice $VMHD
+    Set-VMProcessor -VMName $vmName -Count $cpuCore
     Set-VMFirmware -VMName $vmName -EnableSecureBoot On
-    Start-VM -VMName $vmName
+    $VMInfo = Start-VM -VMName $vmName
+
+    #启动虚拟机
     Start-Process -FilePath "vmconnect.exe" -ArgumentList $('"' + $env:COMPUTERNAME + '"'), $('"' + $vmName + '"')
 }
 
